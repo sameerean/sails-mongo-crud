123e667182f8c409cbf3277aa4b49d97e8d81370
Shameer Kunjumohamed <sameerean@gmail.com>
2014-12-11 14:56:08 +0400

    Improved

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -5,60 +5,84 @@
  * @help        :: See http://links.sailsjs.org/docs/controllers
  */
 
-module.exports = {
-	
-
-
-  /**
-   * `UserController.create()`
-   */
-  create: function (req, res) {
-    return res.json({
-      todo: 'create() is not implemented yet!'
-    });
-  },
-
-
-  /**
-   * `UserController.update()`
-   */
-  update: function (req, res) {
-    return res.json({
-      todo: 'update() is not implemented yet!'
-    });
-  },
-
-
-  /**
-   * `UserController.delete()`
-   */
-  delete: function (req, res) {
-    return res.json({
-      todo: 'delete() is not implemented yet!'
-    });
-  },
-
-
-  /**
-   * `UserController.find()`
-   */
-  find: function (req, res) {
-    return res.view("user/view", {
-      status: 'Error',
-      statusDescription: 'Not implemented yet',
-      title: 'User Details'
-    });
-  },
-
-
-  /**
-   * `UserController.findall()`
-   */
-  findall: function (req, res) {
-    return res.view("user/list", {
-      status: 'OK',
-      title: 'List of users'
-    });
-  }
+module.exports = UserController = {
+    usersList: [
+        {
+            userName: "angeldoll",
+            firstName: "Angel",
+            lastName: "Doll",
+            role: "Admin"
+        },
+        {
+            userName: "battledoom",
+            firstName: "Battle",
+            lastName: "Doom",
+            role: "User"
+        },
+        {
+            userName: "romeo",
+            firstName: "Romeo",
+            lastName: "Romeo",
+            role: "User"
+        },
+        {
+            userName: "geniusgeneral",
+            firstName: "Genius",
+            lastName: "General",
+            role: "User"
+        },
+        {
+            userName: "kunningking",
+            firstName: "Kunning",
+            lastName: "King",
+            role: "User"
+        },
+    ],
+    /**
+     * `UserController.create()`
+     */
+    create: function (req, res) {
+        return res.json({
+            todo: 'create() is not implemented yet!'
+        });
+    },
+    /**
+     * `UserController.update()`
+     */
+    update: function (req, res) {
+        return res.json({
+            todo: 'update() is not implemented yet!'
+        });
+    },
+    /**
+     * `UserController.delete()`
+     */
+    delete: function (req, res) {
+        return res.json({
+            todo: 'delete() is not implemented yet!'
+        });
+    },
+    /**
+     * `UserController.find()`
+     */
+    find: function (req, res) {
+        return res.view("user/view", {
+            status: 'Error',
+            statusDescription: 'Not implemented yet',
+            title: 'User Details'
+        });
+    },
+    /**
+     * `UserController.findall()`
+     */
+    findall: function (req, res) {
+        console.log("UserController = " + JSON.stringify(UserController));
+        console.log("Users = " + JSON.stringify(UserController.userList));
+        return res.view("user/list", {
+            status: 'OK',
+            title: 'List of users',
+            users: UserController.usersList
+        });
+    }
 };
 
